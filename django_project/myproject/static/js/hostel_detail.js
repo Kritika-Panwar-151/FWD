7f584b0652cd8ccfc4cfa40f3a8775c0473ef34b
krati2711 <kratigoel.cs24@bmsce.ac.in>
2025-12-17 09:45:36 +0530

    abcd

---
+++ django_project/myproject/static/js/hostel_detail.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  if (window.lucide) lucide.createIcons();
+});
+
+function openMap() {
+  window.open("https://www.google.com/maps/search/BMSCE", "_blank");
+}
