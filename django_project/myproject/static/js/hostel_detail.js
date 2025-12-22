ec11dba19228661411da8aa7d51443e87e37c863
Kritika-Panwar-151 <kritikapanwar.cs24@bmsce.ac.in>
2025-12-22 22:27:41 +0530

    updated girls hostel page

--- django_project/myproject/static/js/hostel_detail.js
+++ django_project/myproject/static/js/hostel_detail.js
@@ -5,3 +5,18 @@ document.addEventListener("DOMContentLoaded", () => {
 function openMap() {
   window.open("https://www.google.com/maps/search/BMSCE", "_blank");
 }
+
+document.querySelectorAll(".gallery-item").forEach(item => {
+  let timer;
+
+  item.addEventListener("mouseenter", () => {
+    timer = setTimeout(() => {
+      item.classList.add("zoomed");
+    }, 500); // 2 seconds
+  });
+
+  item.addEventListener("mouseleave", () => {
+    clearTimeout(timer);
+    item.classList.remove("zoomed");
+  });
+});
