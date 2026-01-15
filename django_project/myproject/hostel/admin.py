3805b48256454e5316aa4878c0dd59ae281d831c
Kritika-Panwar-151 <kritikapanwar.cs24@bmsce.ac.in>
2026-01-15 22:14:35 +0530

    added in admin

--- django_project/myproject/hostel/admin.py
+++ django_project/myproject/hostel/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Profile, ContactMessage, Booking
 
-
+admin.site.register(Profile)
+admin.site.register(ContactMessage)
+admin.site.register(Booking)
